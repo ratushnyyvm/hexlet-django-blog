57e9acad65b75d2ae685874de4eff6d825770cd8
ratushnyyvm <ratushnyyvm@gmail.com>
2022-11-19 12:02:10 +0300

    Add the actions of Lesson 6

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('article')
+    return render(
+        request,
+        'article/index.html',
+        context={
+            'app_name': 'Article',
+        }
+    )
