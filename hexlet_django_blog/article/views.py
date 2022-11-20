e854b0ac2bc74c2b5aed01361e679a842aac3b82
ratushnyyvm <ratushnyyvm@gmail.com>
2022-11-20 17:46:13 +0300

    Add the actions of Lesson 10

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views import View
 
 
 def index(request):
-    return render(
-        request,
-        'article/index.html',
-        context={
-            'app_name': 'Article',
-        }
-    )
+    return redirect('article', tags='python', article_id=42)
+
+
+class IndexView(View):
+
+    def get(self, request, tags, article_id):
+        if tags and article_id:
+            return render(
+                request,
+                'article/index.html',
+                context={
+                    'tags': tags,
+                    'article_id': article_id,
+                }
+            )
